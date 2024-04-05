d9acc800a2d73474ec5af62782b1d0e9b34d442d
Werlen Santos <werlen.santos@codificar.com.br>
2024-04-05 10:56:20 -0300

    finalizando ferramentas basicas

--- script.js
+++ script.js
@@ -2,23 +2,90 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const inputColor = document.querySelector('.input__color');
-const tools = document.querySelector('.button__tool');
-const sizeButtons = document.querySelector('.button__size');
+const tools = document.querySelectorAll('.button__tool');
+const sizeButtons = document.querySelectorAll('.button__size');
 const buttonClear = document.querySelector('.button__clear');
 
 let brushSize = 10;
-ctx.fillStyle = "#000";
 
-canvas.addEventListener("mousedown", (event) => {
-    const { clientX, clientY } = event;
+let isPainting = false;
 
-    draw(clientX, clientY);
+let activeTool = "rubber";
+
+inputColor.addEventListener("change", ({ target }) => {
+    ctx.fillStyle = target.value;
+})
+
+canvas.addEventListener("mousedown", ({ clientX, clientY }) => {
+    isPainting = true;
+
+    if (activeTool == "brush") {
+        draw(clientX, clientY);
+    }
+
+    if (activeTool == "rubber") {
+        erase(clientX, clientY);
+    }
+})
+
+canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
+    if (isPainting) {
+        if (activeTool == "brush") {
+            draw(clientX, clientY);
+        }
+
+        if (activeTool == "rubber") {
+            erase(clientX, clientY);
+        }
+    }
+})
+
+canvas.addEventListener("mouseup", () => {
+    isPainting = false;
 })
 
 const draw = (x, y) => {
-
-    ctx.fillRect(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize, brushSize);
+    ctx.globalCompositeOperation = "source-over";
+    ctx.beginPath();
+    ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize / 2, 0, 2 * Math.PI);
+    ctx.fill()
 }
 
-draw(100, 100)
-/**29 */
+const erase = (x, y) => {
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.beginPath();
+    ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize / 2, 0, 2 * Math.PI);
+    ctx.fill()
+}
+
+const selectTool = ({ target }) => {
+    const selectedTool = target.closest("button");
+    const action = selectedTool.getAttribute("data-action");
+
+    if (action) {
+        tools.forEach((tool) => { tool.classList.remove('active') });
+        selectedTool.classList.add('active');
+        activeTool = action
+    }
+}
+
+const selectSize = ({ target }) => {
+    const selectedTool = target.closest("button");
+    const size = selectedTool.getAttribute("data-size");
+
+    sizeButtons.forEach((tool) => { tool.classList.remove('active') });
+    selectedTool.classList.add('active');
+    brushSize = size;
+}
+
+tools.forEach((tool) => {
+    tool.addEventListener("click", selectTool);
+})
+
+sizeButtons.forEach((button) => {
+    button.addEventListener("click", selectSize);
+})
+
+buttonClear.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
